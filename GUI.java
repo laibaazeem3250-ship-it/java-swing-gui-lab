0c922284829ca56f192fd02b520f36dc7686365e
Laiba Azeem <laibaazeem3250@gmail.com>
2026-04-07 09:25:49 +0500

    Add files via upload

---
+++ GUI.java
@@ -0,0 +1,18 @@
+package mygui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI extends JFrame {
+
+ public GUI() {
+     super("I've been framed!");
+     setBounds(0, 0, 300, 300);
+ }
+
+ public static void main(String[] args) {
+     JFrame frame = new GUI();
+     frame.setVisible(true);
+     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ }
+}
